cf76fa621b489731b5c79eb42f5fe32174cf2de8
murilothom <murilothom.2002@hotmail.com>
2022-07-14 19:38:32 -0300

    commit final

--- js/formulario.js
+++ js/formulario.js
@@ -19,7 +19,7 @@ formulario.addEventListener("submit", async (e) => {
       return;
     }
     formulario.innerHTML =
-      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317a00;'>Mensagem enviada com sucesso.</span></p>";
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317a00;'>Enviado com sucesso.</span></p>";
     console.log(res);
   } catch (e) {
     console.log(e);
